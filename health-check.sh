2177d5240de17d8357e8eb83d0915395dcf7f837
FireBug-AI <129939220+FireBug-AI@users.noreply.github.com>
2023-11-16 13:58:30 +0700

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -7,15 +7,16 @@ if [[ $origin == *statsig-io/statuspage* ]]
 then
   commit=false
 fi
-
+# ---------
 KEYSARRAY=()
 URLSARRAY=()
-
 urlsConfig="./urls.cfg"
+
 echo "Reading $urlsConfig"
+
 while read -r line
 do
-  echo "  $line"
+  echo " $line"
   IFS='=' read -ra TOKENS <<< "$line"
   KEYSARRAY+=(${TOKENS[0]})
   URLSARRAY+=(${TOKENS[1]})
@@ -30,32 +31,38 @@ for (( index=0; index < ${#KEYSARRAY[@]}; index++))
 do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
-  echo "  $key=$url"
+  echo " $key=$url"
 
-  for i in 1 2 3 4; 
-  do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+  for i in 1 2 3 4; do
+    responseDetails=$(curl --write-out "HTTPCode:%{http_code};HTTPConnect:%{http_connect};NumConnects:%{num_connects};SizeDownload:%{size_download};SpeedDownload:%{speed_download}" --silent --output /dev/null $url)
+    response=$(echo $responseDetails | grep -oP '(?<=HTTPCode:)\d+')
+
     if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
       result="success"
     else
       result="failed"
+      # If failed, log the response details
+      echo "Response details: $responseDetails" >> "logs/${key}_report.log"
     fi
+
     if [ "$result" = "success" ]; then
       break
     fi
     sleep 5
   done
+
   dateTime=$(date +'%Y-%m-%d %H:%M')
+
   if [[ $commit == true ]]
   then
     echo $dateTime, $result >> "logs/${key}_report.log"
-    # By default we keep 2000 last log entries.  Feel free to modify this to meet your needs.
+    # By default we keep 2000 last log entries. Feel free to modify this to meet your needs.
     echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
   else
-    echo "    $dateTime, $result"
+    echo " $dateTime, $result"
   fi
 done
-
+# ---------
 if [[ $commit == true ]]
 then
   git config --global user.name 'duc nguyen'
